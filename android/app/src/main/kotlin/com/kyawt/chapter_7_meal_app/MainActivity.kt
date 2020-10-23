3d89a8c1a3b8937c8ca8ac75a494a353662def0b
Your Name <kyawtkyawtsan1998@gmail.com>
2020-10-23 16:28:49 +0630

    meal app

---
+++ android/app/src/main/kotlin/com/kyawt/chapter_7_meal_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kyawt.chapter_7_meal_app
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
